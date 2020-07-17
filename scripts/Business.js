d74ac98a53e2f6b4cfcd14da8be5a41073ba9af6
jamesnitz <jamesanitz2@gmail.com>
2020-07-17 13:41:04 -0500

    I styled this thing

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,7 +1,7 @@
 export const Business = (bizObj) => {
   return `
-    <section>
-      <h5>${bizObj.companyName}</h5>
+    <section class="biz__card">
+      <h2>${bizObj.companyName}</h2>
       <div>${bizObj.addressFullStreet}</div>
     </section>
   `
